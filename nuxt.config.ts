4bc3a679a0b496e91b9c3dce044491c4d83ac46d
f <farhad.3.rohani@gmail.com>
2025-07-09 10:40:29 +0330

    install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  css: [
-    'bootstrap/dist/css/bootstrap.css',
-    'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-  ],
-  plugins: [
-      '@/plugins/bootstrap.js',
-    { src: '@/plugins/localStorage.client.js', mode: 'client' }
-
-  ]
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    modules: [
+        '@nuxt/eslint',
+        '@bootstrap-vue-next/nuxt'
+    ],
+    css: [
+        'bootstrap/dist/css/bootstrap.css',
+        'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+    ],
+    plugins: [
+        // {src: '@/plugins/localStorage.client.js', mode: 'client'}
+        { src: '~/plugins/firebase.js', mode: 'client' },
+    ],
+    pages: true,
+    // debug: true
 })
