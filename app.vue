63779697622e4ae18e41b8ac404c8680d727e2e1
f <farhad.3.rohani@gmail.com>
2025-07-09 10:40:56 +0330

    notifications

--- app.vue
+++ app.vue
@@ -1,6 +1,62 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtPage/>
+  <b-toast
+      v-model:show="show"
+      :title="notification.title"
+      :body="notification.message"
+      :variant="notification.variant"
+      dir="rtl"
+      solid
+      show-on-pause
+      :auto-hide-delay="5000"
+      :progress-props="{
+    showProgress: true,
+    animated: true,
+    striped: true,
+    variant: notification.variant
+  }"
+      class="position-fixed top-0 end-0 m-3 z-50 text-end"
+      @hidden="handleHidden"
+  />
 </template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    ...mapGetters('notifications', ['notification']),
+  },
+  watch: {
+    'notification.message': {
+      immediate: true,
+      handler(val) {
+        if (val) {
+          this.show = true
+        }
+      }
+    }
+  },
+  methods: {
+    ...mapActions('notifications', ['clearNotice']),
+    handleHidden() {
+      this.show = false
+      this.clearNotice()
+    }
+  }
+}
+</script>
+<style>
+button.btn-close.ms-auto {
+  margin-left: 0 !important;
+}
+
+.toast-header {
+  justify-content: space-between;
+}
+</style>
---
+++ store/notifications.js
@@ -0,0 +1,54 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        message: null,
+        title: null,
+        variant: 'info',
+    }),
+    mutations: {
+        SET_NOTIFICATION(state, { message, title = 'پیام سیستم', variant = 'info' }) {
+            console.log(message, title, variant)
+            state.message = message
+            state.title = title
+            state.variant = variant
+        },
+        CLEAR_NOTIFICATION(state) {
+            state.message = null
+            state.title = null
+            state.variant = 'info'
+        }
+    },
+    actions: {
+        notify({commit}, payload) {
+            commit('SET_NOTIFICATION', payload)
+            // بعد از چند ثانیه پاک شود (مثلاً auto-hide)
+            setTimeout(() => {
+                commit('CLEAR_NOTIFICATION')
+            }, 5000)
+        },
+        notifySuccess({commit}, message) {
+            commit('SET_NOTIFICATION', {
+                message:message,
+                title:'موفقیت',
+                variant:'success'
+            })
+        },
+        notifyError({commit},  message) {
+            commit('SET_NOTIFICATION', {
+                message:message,
+                title:'خطا',
+                variant:'danger'
+            })
+        },
+        clearNotice({commit}) {
+            commit('CLEAR_NOTIFICATION')
+        }
+    },
+    getters: {
+        notification: (state) => ({
+            message: state.message,
+            title: state.title,
+            variant: state.variant,
+        })
+    }
+}
